622b0082cde42a2fc4d5b71aab742c32ce980143
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-12-05 08:36:57 -0600

    Initial Commit

---
+++ Attack on trivia/Attack_on_triviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Attack_on_triviaApp.swift
+//  Attack on trivia
+//
+//  Created by Krow on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Attack_on_triviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Attack on trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Attack on trivia
+//
+//  Created by Krow on 12/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
